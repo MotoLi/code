72d68b7cb29d230fda6f3b9a978e7197c13b86b0
MotoLi <2098537060@qq.com>
2018-04-08 21:12:07 +0800

    20180408

---
+++ python学习/benbanfa/02/projects/skeleton/setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': '尼达耶胶囊',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': '2098537060@qq.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex47'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
